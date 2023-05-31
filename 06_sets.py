9641c97cd15d10f2d5674e0d30416149a0b303a8
Zeke16 <kr2000.16@gmail.com>
2023-05-31 17:58:08 -0600

    Diccionarios y sets

--- 06_sets.py
+++ 06_sets.py
@@ -1,7 +1,7 @@
 # SETS
 
 my_set = set()
-my_other_set = {}
+my_other_set = {} # de esta manera se declaran tambien los diccionarios
 
 print(type(my_set))
 print(type(my_other_set))
---
+++ 07_dicts.py
@@ -0,0 +1,11 @@
+# Diccionarios
+
+my_dict = dict()
+my_other_dict = {}
+
+print(type(my_dict))
+print(type(my_other_dict))
+
+my_other_dict = {"nombre": "Ezequiel", "edad": 23, "carrera": "Software"}
+
+print(my_other_dict)
